42656a3b9f54ebf20f83b92f930afb394d6664a1
Aleksei Slesarev <aleksei@Mac-mini-Aleksei.local>
2018-09-26 08:03:58 +0700

    prepearing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<h1>Hello</h1>)
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
